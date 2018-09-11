7c80c5e4124aea5edc8106679216712f2cbb0946
kodomo898 <kodomo893@gmail.com>
2018-09-11 23:31:59 +0900

    initial commit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
